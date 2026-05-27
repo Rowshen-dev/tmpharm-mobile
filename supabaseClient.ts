eeb0b69c2cbf323acc8cfa5762748f406ddf6004
Rowshen-dev <hojageldiyev@icloud.com>
2026-05-28 02:17:32 +0500

    feat: new supabase connection

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL='https://vkgdfegqhbrpzehhfbfh.supabase.co'
-const SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZrZ2RmZWdxaGJycHplaGhmYmZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQzNjUzMzcsImV4cCI6MjA4OTk0MTMzN30.ktDl5Y_5SNkOx3vr5VbMpNphr0aUUAPo28yLK3tN-Rc'
+const SUPABASE_URL='https://sqtrvvomaregjuxdqaws.supabase.co'
+const SUPABASE_ANON_KEY='eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNxdHJ2dm9tYXJlZ2p1eGRxYXdzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5MDU4NjAsImV4cCI6MjA5NTQ4MTg2MH0.Lv88DVnOcIeHQxLZl0PDP_waRaX1J6swHqjjEoMY22k'
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
